b7145d39259161cd0aa292b4af01cd0d9757b493
Fatima El Orch <fatima.elorch@clermont.in2p3.fr>
2017-04-07 14:15:02 +0200

    client_PUB-SUB

--- client.c
+++ client.c
@@ -1,23 +1,46 @@
-#include <zmq.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
+/******************************************************/
+/*		client qui recupere 100 fois les donnees      */
+/*		les donnees envoyees par le serveur un boucle */
+/*		recupere le code la temperature et l'humidite */
+/*      et calcule la temperature moyenne  			  */
+/*                  et l'affiche                      */
+/******************************************************/
+#include "zhelper.h"
 
-int main(void){
-	printf("connexion a Hello World ...\n");
-	void * ctx =zmq_ctx_new();// creation d'un contexte
-	void * req =zmq_socket(ctx, ZMQ_REQ);//creation d'une socket
-	zmq_connect(req,"tcp://localhost:5555");//connection du client 
-	int req_nbr;
-	for(req_nbr =0;req_nbr !=10;req_nbr++){//envoie du mot Hello 10 fois 
-		char buffer[5];
-		printf("envoie du hello numero : %d\n",req_nbr);
-		zmq_send(req,"Hello",5,0);
-		zmq_recv(req,buffer,5,0);
-		printf("mot recu: %s\n",buffer);//affichage des donnees recu du serveur 
-		
+
+int main(int argc,char *argv[]){
+
+	printf("collection des donnees relatives à la temperature\n ");
+	void*ctx = zmq_ctx_new();
+	void * sub= zmq_socket(ctx,ZMQ_SUB);
+	zmq_setsockopt(sub,ZMQ_SUBSCRIBE,"",0);
+	//mq_setsockopt(zmq_RCVHWM,0,,);
+	int rc = zmq_connect(sub,"tcp://localhost:2223");
+	
+	assert(rc == 0);
+	const char *filter= (argc >1)? argv[1]:"10001";
+	rc = zmq_setsockopt(sub,ZMQ_SUBSCRIBE,filter,strlen(filter));
+	assert(rc ==0);
+	printf("connexion\n");
+	int update_nbr;
+	long total_temp =0;
+	int code , temp,humid;
+	for(update_nbr =0;update_nbr <100;update_nbr++){
+		//char car[20];
+		char*car =  s_recv(sub);
+		car[strlen(car)-1]='\0';
+		//zmq_recv(sub,car,20,0);
+		printf("chaine de caractère recue : %s\n\n",car);
+		sscanf(car,"%d %d %d",&code ,&temp,&humid);
+		total_temp+=temp;
+		temp++;
+		free(car);
 	}
-	zmq_close(req);//fermeture de la requete
+	printf("la temperatur moyenne pour le zipcode '%s' (%d) est %dF\n",filter,code,(int)(total_temp / update_nbr));
+	zmq_close(sub);
 	zmq_ctx_destroy(ctx);
+	
+	
 	return 0;
 }
+//gcc -o client -lzmq client.c && clear && ./client 63000
